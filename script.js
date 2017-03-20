04247ee469095024b62000b3e8467c51f93b9dd1
Cindy Romero <romerocindy22@gmail.com>
2017-03-20 14:17:40 +0000

    made a semi house

--- script.js
+++ script.js
@@ -12,7 +12,32 @@ right(90);
 forward(50);
 }
 
+function triangle() { 
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+}
+
+function rectangle() {
+    forward(70);
+    right(90);
+    forward(120);
+    right(90);
+    forward(70);
+    right(90);
+    forward(120);
+    right(90);
+    forward(80);
+    right(120);
+    forward(95);
+    right(120);
+    forward(95);
+    
+}
+    
 // Type your function call below
-
-
+rectangle();
 stamp();
